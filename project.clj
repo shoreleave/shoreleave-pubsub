bd56a35306a82793057cc3764a720739cc8db265
Paul deGrandis <paul.degrandis@gmail.com>
2012-06-18 15:25:17 -0400

    project update

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject shoreleave-pubsub "0.2.1"
+(defproject shoreleave/shoreleave-pubsub "0.2.1"
   :description "A smarter client-side with ClojureScript : Shoreleave's publish/subscribe system"
   :url "http://github.com/shoreleave"
   :license {:name "Eclipse Public License - v 1.0"
@@ -9,10 +9,8 @@
                  [shoreleave/shoreleave-core "0.2.1"]
                  [shoreleabe/shoreleave-browser "0.2.1"]
                  [shoreleabe/shoreleave-worker "0.2.1"]]
-  :dev-dependencies [[vimclojure/server "2.3.3" :exclusions [org.clojure/clojure]] 
-                     ;[cdt "1.2.6.2-SNAPSHOT"]
+  :dev-dependencies [;[cdt "1.2.6.2-SNAPSHOT"]
                      ;[lein-cdt "1.0.0"] ; use lein cdt to attach
                      ;[lein-autodoc "0.9.0"]
-                     [lein-marginalia "0.7.1"]]
-  :plugins  [[lein-cljsbuild "0.2.1"]])
+                     [lein-marginalia "0.7.1"]])
 
